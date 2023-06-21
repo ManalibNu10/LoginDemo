f007300d3341d6fdf015e228ecb204d59b16a5e6
ManalibNu10 <manalib@nu10.co>
2023-06-21 13:58:36 +0530

    Initial commit

---
+++ LoginDemo/LoginDemo/LoginDemo.h
@@ -0,0 +1,18 @@
+//
+//  LoginDemo.h
+//  LoginDemo
+//
+//  Created by Manali Bobade on 21/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoginDemo.
+FOUNDATION_EXPORT double LoginDemoVersionNumber;
+
+//! Project version string for LoginDemo.
+FOUNDATION_EXPORT const unsigned char LoginDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginDemo/PublicHeader.h>
+
+
